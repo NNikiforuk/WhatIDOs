a601eaa87dc5c3e26b61b560906bc5ad742ff77d
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-20 19:12:17 +0200

    Initial Commit

---
+++ whatIDOs/whatIDOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatIDOsApp.swift
+//  whatIDOs
+//
+//  Created by Natalia Nikiforuk on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct whatIDOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
