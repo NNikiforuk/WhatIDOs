13a2ce5d3e22f4936759efb198f5e754b14d97bb
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-23 14:26:14 +0200

    Test with database

---
+++ whatIDOs/Task.swift
@@ -0,0 +1,18 @@
+//
+//  Task.swift
+//  whatIDOs
+//
+//  Created by Natalia Nikiforuk on 22/10/2024.
+//
+
+import SwiftUI
+
+struct Task: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Task()
+}
